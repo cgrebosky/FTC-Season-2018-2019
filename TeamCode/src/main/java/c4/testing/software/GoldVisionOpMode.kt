c1e1bbe590067ab13aefc0a4600c01883aab70cd
cgrebosky <cgrebosky@gmail.com>
2018-10-11 17:06:32 -0700

    Added README & moved a single file to clean up project

--- TeamCode/src/main/java/c4/testing/software/GoldVisionOpMode.kt
+++ TeamCode/src/main/java/c4/testing/software/GoldVisionOpMode.kt
@@ -1,10 +1,11 @@
-package c4.prod.vision
+package c4.testing.software
 
+import c4.prod.vision.GoldVision
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.corningrobotics.enderbots.endercv.CameraViewDisplay
 
-@TeleOp(name = "GoldVisionOpMode", group = "Diagnostics")
+@TeleOp(name = "GoldVisionOpMode", group = "Testing")
 class GoldVisionOpMode: OpMode() {
     var goldVision: GoldVision? = null
 
