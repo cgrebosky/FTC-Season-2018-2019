0991b1f03744e4248315e41a07e9a85265e8367f
cgrebosky <cgrebosky@gmail.com>
2018-10-16 15:12:01 -0700

    Make the subsystem stuff & implement it for tank drive.

---
+++ TeamCode/src/main/java/c4/diagnostic/RotationDiagnostic.kt
@@ -0,0 +1,17 @@
+package c4.diagnostic
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+@TeleOp(name = "Rotation Diagnostic", group = "Diagnostics")
+class RotationDiagnostic: OpMode() {
+    override fun init() {
+
+    }
+
+    override fun loop() {
+
+    }
+
+}
